0e171c7339e6f32f131b5918054de8743cc0a966
leahminsky <51495567+leahminsky@users.noreply.github.com>
2021-11-21 16:18:27 -0500

    Update sketch_my_cool_project.py
    Updated to list of lists, cleaned up code/moved some stuff out of this file

--- sketch_my_cool_project.py
+++ sketch_my_cool_project.py
@@ -1,6 +1,6 @@
 #CANNOT RUN UNLESS CONNEECTED TO AXIDRAW
 
-import sys
+import sys, math
 
 from cmu_112_graphics import *
 from pyaxidraw import axidraw
@@ -22,30 +22,7 @@ canvasHeight = 680
 mouseScaleFactor = 85
 margin = 5
 ######################################################################
-#make draw features into classes maybe? or maybe just make the main a class
 
-#example of a dynamic gui element
-class Button(object):
-    def __init__(self, label, color, topLeftX, topLeftY):
-        self.label = label
-        self.color = color
-        self.topLeftX = topLeftX
-        self.topLeftY = topLeftY
-
-    def buttonLocation(self):
-        return self.topLeftX, self.topLeftY
-
-    def buttonSettings(self):
-        text = self.label
-        buttonWidth = len(text)*10 #find a ratio num so button size is dynamic
-        buttonHeight = 20
-        buttonColor = self.color
-        return buttonWidth, buttonHeight, buttonColor
-
-    def buttonOutline(self, outCol):
-        buttonOutlineWidth = 5
-        buttonOutlineColor = "blue"
-        return buttonOutlineWidth, buttonOutlineColor
 
 def inBounds(x, y):
     if (x > 0 and x < (canvasWidth-margin) and y > 0 
@@ -53,54 +30,46 @@ def inBounds(x, y):
         return True
     return False
 
+def constrain(x, y):
+    constrainedX = x/mouseScaleFactor
+    constrainedY = y/mouseScaleFactor
+    return constrainedX, constrainedY
+
+
 def appStarted(app):
     app.mouseDrag = False
     app.mousePress = False
+    app.userJustWent = False
 
-    app.x1, app.y1 = 0, 0
-    app.x2, app.y2 = 0, 0
-
-    app.linePos = [ ]
-    app.num = len(app.linePos)
-
-    #making button ex.
-    app.zigZagButton = Button("Zig Zag Line", "green", 10, 10)
+    app.lines = [ ]
+    # app.currLine = [ ]
+    app.numLines = len(app.lines)
+    
 
 def keyPressed(app, event):
-    if event.key == 'h': #moves to origin
+    if event.key == 'h':
+        # ad.penUp() #lift pen then move to origin
         # ad.moveto(0, 0)
         pass
 
 def mousePressed(app, event):
     app.mousePress = True
-    if inBounds(event.x, event.y): 
-        app.linePos.append((event.x, event.y))
-        print(app.linePos)
-        app.x1, app.y1 = app.linePos[0]
-        app.x2, app.y2 = app.linePos[0]
+    app.mouseDrag = False
+    currLine = [ ]
+    if inBounds(event.x, event.y):
+        app.lines.append(currLine)
+        currLine.append((event.x, event.y))
+        app.userJustWent = True
+
         # ad.pendown()
-
-        #need to make functionality for when a button is clicked it changes 
-        #the drawing tool to that feature
-        # if (zigZagButtonSelected):
-        #     ad.lineto((newX)/mouseScaleFactor, (newY)/mouseScaleFactor) 
-# import math
-# class Coordinate(object):
-#     def __init__(self, x,y):
-#         self.x = x
-#         self.y = self
-
-#     def __repr__(self):
-#         return f"{self.x} + , {self.y}"
-
-#     def angle(self):
-#         return math.atan2(self.y, self.x)
-
-def perpindicular(coord):
-    #calculate perp
 
 def mouseReleased(app, event):
     app.mousePress = False
+    app.mouseDrag = False
+    app.userJustWent = True
+    
+    #change the pen speed when axidraw needs to cover a large surface area?
+    #like in golan's example
     # ad.penup()
 
 def mouseMoved(app, event):
@@ -109,47 +78,86 @@ def mouseMoved(app, event):
         # ad.moveto((event.x)/mouseScaleFactor, (event.y)/mouseScaleFactor)
         pass
 
+
 def mouseDragged(app, event):
-    newX, newY = 0, 0
-    zigZag = 0
+    app.mousePress = False
     app.mouseDrag = True
+    app.userJustWent = True
+
+    currLine = [ ]
     if inBounds(event.x,event.y):
-        app.linePos.append((event.x, event.y))
-        for i in range(1, len(app.linePos)):
-                app.linePos[i] = app.linePos[i-1]
-                # newX, newY = drawFeature1(app.linePos[i], zigZag) 
-        # ad.lineto((event.x)/mouseScaleFactor, (event.y)/mouseScaleFactor)
+        if (len(currLine) > 0):
+            currLine.append((event.x, event.y))
+        else: #or object?
+            app.lines.append(currLine)
+            currLine.append((event.x, event.y))
         
-def drawFeature1(curr, zigZag):
-    #zigzag line, this is just an example not gonna actually use
-    currX, currY = curr[0], curr[1]
-    if (zigZag == 0):
-        currX, currY
-        zigZag = 1
-        zigZagX = currX + 20
-    else:
-        zigZag = 0
-        zigZagY = currY + 20
 
-    return zigZagX, zigZagY
+def userDraw(app, canvas):
+    currLine = app.lines[app.numLines]
 
-#at the moment I cannot figure out why my drawing line is not 
-#appearing on the canvas
+    if (app.mouseDrag):
+        for i in range(app.numLines):
+            currLine = app.lines[i]
+            for j in range(1, len(currLine)):
+                currPointX, currPointY = currLine[j]
+                lastPointX, lastPointY = currLine[j-1]
+
+                canvas.create_line(currPointX, currPointY, 
+                        lastPointX, lastPointY, 
+                        width = 5, fill = "black", smooth="true")
+                        
+                currPointX, currPointY = lastPointX, lastPointY
+
+    if (app.mousePress):
+        for i in range(app.numLines):
+            currLine = app.lines[i]
+            currPointX, currPointY = currLine[i][0]
+        
+            canvas.create_oval(currPointX, currPointY, 
+                            currPointX+2, currPointY+2, 
+                            fill = "black")
+
+def robotDraw(app, canvas):
+    currLine = app.lines[app.numLines]
+    numLinePoints = len(currLine)
+
+    #### debug view ########### 
+    for i in range(numLinePoints):
+        currPoint = currLine[i]
+        lastPoint = currLine[i-1]
+        currPointX, currPointY = (currPoint[0]+10, currPoint[1] - 20)
+        lastPointX, lastPointY = (lastPoint[0]+10, lastPoint[1] - 20)
+        canvas.create_line(currPointX, currPointY, 
+                        lastPointX, lastPointY, 
+                        width = 5, fill = "red", smooth="true")
+        
+        currPoint = lastPoint
+    ##############
+
+    # if (numLinePoints > 0):
+    #     desiredAxiPoint = currLine[numLinePoints]
+    #     moveAxiHere(desiredAxiPoint)
+        #ad.lineto(desiredAxiPoint.x, desiredAxiPoint.y)
+
+def moveAxiHere(goPoint):
+    mX, mY = constrain(goPoint[0], goPoint[1])
+    return mX, mY
+    # ad.moveTo(mX, mY)
+
+
 def redrawAll(app, canvas):
-    x, y = app.zigZagButton.buttonLocation()
-    width, height, col = app.zigZagButton.buttonSettings()
-    outlineWidth, outlineCol = app.zigZagButton.buttonOutline("pink")
-    canvas.create_rectangle(x, y, width, height, fill = col, 
-                    outline = outlineCol, width = outlineWidth)
-    # print(app.mouseDrag, app.mousePress)
-    # if (app.mouseDrag):
-    for i in range(1, len(app.linePos)-1):
-        canvas.create_line(app.linePos[i][0], app.linePos[i][1], 
-                app.linePos[i-1][0], app.linePos[i-1][1], 
-                width = 5, fill = "black")
-# elif (app.mousePress):
-    canvas.create_line(app.x1, app.y1, app.x2, app.y2, 
-                        width = 5, fill = "black")
+    print(app.lines)
+    if (app.mouseDrag):
+        userDraw(app, canvas)
+    elif (app.mousePress):
+        userDraw(app, canvas)
+    elif (app.userJustWent and not app.mouseDrag and not app.mousePress):
+        robotDraw(app, canvas)
+    else:
+        #do nothing
+        # ad.penUp()
+        pass
 
 def runAxi():
     runApp(width = canvasWidth, height = canvasHeight)
